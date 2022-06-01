c23e2fd45effb2ef190bbae5a66ba2cfb8ce71fe
heekng <rhkd1769@gmail.com>
2022-06-01 23:31:47 +0900

    Spring Converter 적용

---
+++ src/main/java/com/heekng/typeconverter/WebConfig.java
@@ -0,0 +1,22 @@
+package com.heekng.typeconverter;
+
+import com.heekng.typeconverter.converter.IntegerToStringConverter;
+import com.heekng.typeconverter.converter.IpPortToStringConverter;
+import com.heekng.typeconverter.converter.StringToIntegerConverter;
+import com.heekng.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+
+}
